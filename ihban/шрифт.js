de335af1df9307968674fed714d8559a8e95e419
SHAARON228 <129669155+SHAARON228@users.noreply.github.com>
2023-05-21 17:50:00 +0300

    Create шрифт.js

---
+++ ihban/шрифт.js
@@ -0,0 +1,11 @@
+function text_30px(){
+  document.getElementById('text').style.fontSize = '30px'
+}
+function text_15px(){
+  document.getElementById('text').style.fontSize = '15px'
+}
+function change_style(){
+  document.getElementById('text').style.fontSize = '75px';
+  document.getElementById('text').style.color = 'green';    
+  document.getElementById('text').style.fontStyle = 'Italic';
+}
